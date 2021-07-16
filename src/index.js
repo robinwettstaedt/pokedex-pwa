955b5bafd16b2d7ad7af16283b867ea5c55204da
robinwettstaedt <robinwettstadt@gmail.com>
2021-07-16 14:07:01 +0200

    added all necessary files to make the app a PWA

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { AuthProvider } from './contexts/AuthContext';
 import { ModalProvider } from './contexts/ModalContext';
 import { ThemeChangeProvider } from './contexts/ThemeChangeContext';
 import { SearchProvider } from './contexts/SearchContext';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -20,3 +21,8 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorkerRegistration.register();
